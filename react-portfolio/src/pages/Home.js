fccc5062c1f11cba54cc49abed06aded9e1ffe12
frozehu <166183471+frozehu@users.noreply.github.com>
2025-10-02 16:30:40 -0400

    fixed picture not appearing

--- react-portfolio/src/pages/Home.js
+++ react-portfolio/src/pages/Home.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Home = () => {
   return (
     <section className="hero">
       <div className="hero-content">
         <div className="profile-section">
           <img 
-            src="/images/profile.jpg" 
+            src={`${process.env.PUBLIC_URL}/images/profile.jpg`} 
             alt="Elizabeth Gonzalez" 
             className="profile-photo"
           />
@@ -16,8 +18,8 @@ const Home = () => {
               Currently pursuing Computer Science with a minor in Digital Arts and Sciences.
             </p>
             <div className="hero-buttons">
-              <a href="/projects" className="btn btn-primary">View My Work</a>
-              <a href="/contact" className="btn btn-secondary">Get In Touch</a>
+              <Link to="/projects" className="btn btn-primary">View My Work</Link>
+              <Link to="/contact" className="btn btn-secondary">Get In Touch</Link>
             </div>
           </div>
         </div>
